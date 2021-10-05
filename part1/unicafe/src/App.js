93462ac297422e28b368fd622449336463a17474
jingzhi <jzkoay@gmail.com>
2021-10-05 12:36:19 +0800

    complete unicafe 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClick = (e) => {
+    console.log(e.target.name)
+    if (e.target.name === "good") {
+      setGood(good+1)
+    }
+    else if (e.target.name === "neutral") {
+      setNeutral(neutral+1)
+    }
+    else {
+      setBad(bad+1)
+    }
+  }
+
+  const Button = ({type}) => {
+    return (
+      <button name={type} onClick={(e) => handleClick(e)}>{type}</button>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>Give Feedback</h4>
+      <p>
+        <Button type={"good"} value={good} />
+        <Button type={"neutral"} value={neutral} />
+        <Button type={"bad"} value={bad} />
+      </p>
+      <h4>Statistics</h4>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
-  );
+  )
 }
-
 export default App;
